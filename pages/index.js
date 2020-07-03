1c0efe2ab11c947cf1eeabead4cab9fdeaab994d
romluc <romulofilhofilho@gmail.com>
2020-07-03 11:12:33 -0300

    Set up linking structure

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,23 @@
 import React from 'react';
+import Link from 'next/link';
 
 const Index = () => {
-  return <h1>Hello FSM week!</h1>;
+  return (
+    <div>
+      <h1>Hello FSM week!</h1>
+      <div>
+        <Link href="/about">
+          <a>About</a>
+        </Link>
+        <Link href="/contact">
+          <a>Contact</a>
+        </Link>
+        <Link href="/survey">
+          <a>Survey</a>
+        </Link>
+      </div>
+    </div>
+  );
 };
 
 export default Index;
---
+++ pages/survey.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Link from 'next/link';
+
+const Survey = () => {
+  return (
+    <div>
+      <h1>Survey</h1>
+      <div>
+        <Link href="/">
+          <a>Home</a>
+        </Link>
+      </div>
+    </div>
+  );
+};
+
+export default Survey;
